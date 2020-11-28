5a8bd006037d25a28b50f7d3bca04380540c9fdf
Wonseok Shin <wsshin97@gmail.com>
2020-11-28 00:12:21 -0500

    Minor update in comments

--- src/matrix/differential/divergence.jl
+++ src/matrix/differential/divergence.jl
@@ -44,7 +44,7 @@ function create_divg(isfwd::SBool{K},  # isfwd[w] = true|false: create ∂w by f
     for nw = 1:K  # direction of differentiation
         I, J, V = create_∂info(nw, isfwd[nw], N, ∆l⁻¹[nw], isbloch[nw], e⁻ⁱᵏᴸ[nw])
 
-        nu = permute∂[nw]  # index of input field (index of matrix block)
+        nu = permute∂[nw]  # component of input field to feed to ∂w (index of matrix block)
         jstr, joff = order_cmpfirst ? (K, nu-K) : (1, M*(nu-1))  # (column stride, column offset)
         @. J = jstr * J + joff
 
