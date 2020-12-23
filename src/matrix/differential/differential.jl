63ec32a8a21de8b21f6ca6e50a490dd23f33c635
Wonseok Shin <wsshin97@gmail.com>
2020-12-23 16:47:18 -0500

    Add {matrix,matrixfree}/differential/differential.jl

---
+++ src/matrix/differential/differential.jl
@@ -0,0 +1,9 @@
+# Conventions for Cartesian components
+# nw: direction of differentiation (∂w)
+# nu: component of the input field (Fu)
+# nv: component of the output field (Gv)
+
+include("partial.jl")
+include("curl.jl")
+include("divergence.jl")
+include("gradient.jl")
